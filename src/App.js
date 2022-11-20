970398b0432b6933f7143feac906668351fed49e
sontii <sontii78@gmail.com>
2022-11-20 15:07:27 +0100

    done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
 
   const [running, setRunning] = React.useState(false)
   const [quizData, setQuizData] = React.useState([])
+  const [countAnswer, setCountAnswer] = React.useState(0)
+  const [newGame, setNewGame] = React.useState(false)
+  const [checkAnswer, setCheckAnswer] = React.useState(false);
   
 	function generateAnswers(quiz){
 		//map over single quiz and push answers to new array
@@ -33,7 +36,6 @@ function App() {
 		// shuffle index for quiz buttons
 		let allAnswer = _.shuffle(answers);
 		const newQuiz = { ...quiz, allAnswer };
-
 		return newQuiz
 	}
 
@@ -42,7 +44,7 @@ function App() {
 	//fetch
     const fetchData = async () => {
       try {
-		const apiUrl = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple"
+		const apiUrl = "https://opentdb.com/api.php?amount=4&category=9&difficulty=easy&type=multiple"
 		const response = await fetch(apiUrl);
 		const json = await response.json();
 
@@ -54,7 +56,6 @@ function App() {
 		//mutate JSON merge correct and incorrect array 
 		//with boolean and unique id for buttons
 		const mutateJson = jsonId.map(quiz => generateAnswers(quiz))
-
 		setQuizData(mutateJson);
 		
 		} catch (error) {
@@ -62,33 +63,89 @@ function App() {
 		}
 	};
 	fetchData();
-	}, []);
 
-	function selectAnswer() {
-		console.log("clicked")
+	}, [newGame]);
+
+	React.useEffect(() => {
+		//count right answers
+		let counter = 0
+		quizData.map(quiz => quiz.point ? counter++ : null)
+		setCountAnswer(counter)
+		
+	}, [quizData]);
+
+	function selectAnswer(key, id) {
+		const newArray = [...quizData]
+		// slected answer check correct or not, add point to array for count correts
+		const mutate = newArray.map(quiz => {
+			if (quiz.id === id ){
+				quiz.allAnswer.map((answer, index) => {
+					if(answer.keys === key) {
+						answer['isSelected'] = true
+						if(answer.correct === true) {
+							quiz["point"] = true
+						} else {
+							quiz["point"] = false
+						}
+					} else {
+						answer["isSelected"] = false;
+					}
+				})
+			} 
+			return quiz
+			
+		})
+		setQuizData(mutate)
+	}
+	
+	function playGame() {
+		setRunning(true)
+		setNewGame(true)
+	}
+
+	function checkingAnswer() {
+		setCheckAnswer(true)
+	}
+
+	function playAgain() {
+		setCheckAnswer(false)
+		setNewGame(false)
 	}
 
   return (
 		<main>
-			<img className="blob-yellow" src={blob_yellow} alt="just a blob" />
-			<img className="blob-blue" src={blob_blue} alt="just a blob" />
+			<img
+				className={`blob-yellow${running === true ? " shrink-yellow" : ""}`}
+				src={blob_yellow}
+				alt="just a blob"
+			/>
+			<img
+				className={`blob-blue${running === true ? " shrink-blue" : ""}`}
+				src={blob_blue}
+				alt="just a blob"
+			/>
 
-			{running ? 
-				<div className='quiz-all'>
-					<Game 
+			{running ? (
+				<Game
 					quizData={quizData}
-					selectAnswer={()=> selectAnswer()}
-					/>
-				</div>
-			 :
+					selectAnswer={selectAnswer}
+					checkAnswer={checkAnswer}
+					running={running}
+					countAnswer={countAnswer}
+					newGame={newGame}
+					playGame={playGame}
+					checkingAnswer={checkingAnswer}
+					playAgain={playAgain}
+				/>
+			) : (
 				<div className="title">
 					<h3>Quizzical</h3>
 					<p>Some description if needed</p>
-					<button onClick={() => setRunning(true)} className="start-button">
+					<button onClick={() => playGame()} className="start-button">
 						Start quiz
 					</button>
 				</div>
-			}
+			)}
 		</main>
 	);
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -9,17 +9,55 @@ export default function Game(props) {
 					<span>{decode(quiz.question)}</span>
 				</div>
 				<div className="buttons-container">
-					{quiz.allAnswer.map((answ) => {
-						return <button onClick={props.selectAnswer} key={answ.keys}>{decode(answ.answer)}</button>;
-					})}
+					{quiz.allAnswer.map((answ) => (
+						<button
+						// style with classNames if selected or checking the answers
+							className={`answer-button ${
+								answ.isSelected === true ? "button-selected" : ""
+							} 
+							${props.checkAnswer === true && answ.correct ? "button-check-correct" : ""}
+							${
+								props.checkAnswer === true && answ.isSelected && !answ.correct
+									? "button-check-incorrect"
+									: ""
+							}
+							${props.checkAnswer === true && !answ.correct ? "button-check-notCorrect" : ""}
+							`}
+							onClick={() => props.selectAnswer(answ.keys, quiz.id)}
+							key={answ.keys}
+						>
+							{decode(answ.answer)}
+						</button>
+					))}
 				</div>
 				<hr></hr>
 			</div>
 		));
+	
 
     return (
-			<div>
-				{quizElement}
+			<div className="quiz-wrap">
+				<div>{quizElement}</div>
+				<div className="check-container">
+					{props.checkAnswer === false ? (
+						<button onClick={props.checkingAnswer} className="check-button">
+							Check answear
+						</button>
+					) : (
+						<div>
+							<span>
+								You scored {props.countAnswer + " / " + props.quizData.length}{" "}
+								correct answers
+							</span>
+							<button
+								onClick={props.playAgain}
+								className="check-button play-button"
+							>
+								Play again
+							</button>
+						</div>
+					)}
+				</div>
 			</div>
 		);
 };
